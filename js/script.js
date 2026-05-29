94a7d6b101e3dcfae83216c5343ef9b54e0edf21
jirip <jiri.pernik@gmail.com>
2026-05-29 19:36:46 +0200

    version_1.0

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+            }
+        });
+    },
+    { threshold: 0.2 }
+);
+
+document.querySelectorAll('.animate').forEach(el => {
+    observer.observe(el);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.animate').forEach(el => observer.observe(el));
+});
